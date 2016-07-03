b3b7584f24389d2c4c570fb9f7ba72668a30c65e
Luz Frias <luzfrias@gmail.com>
2016-07-03 23:12:41 +0200

    Fix NAs better in metro data

--- R/meteo-data.R
+++ R/meteo-data.R
@@ -11,6 +11,8 @@ library(XML)
 library(lubridate)
 library(data.table)
 
+source("R/util.R")
+
 ##########################################################################
 # Functions
 ##########################################################################
@@ -23,7 +25,7 @@ load_meteo_data <- function() {
 
 download_meteo_data <- function() {
   from <- as.Date("2001-01-01")
-  to <- as.Date("2016-05-31")
+  to <- Sys.Date()
   current <- from
   it <- 0
   
@@ -37,6 +39,8 @@ download_meteo_data <- function() {
                   "&day=",   days,
                   "&hora=",  "00",
                   "&ind=",   "08221") # Madrid - Barajas
+    
+    # Download, parse data and transform it
     html.tables <- readHTMLTable(url, stringsAsFactors = FALSE, skip.rows = 1:2)
     data <- html.tables[[3]][,c(1,4,8,10)]
     colnames(data) <- c("date", "mean_temp", "wind_speed", "rain")
@@ -45,13 +49,8 @@ download_meteo_data <- function() {
     data$mean_temp  <- as.numeric(data$mean_temp)
     data$wind_speed <- as.numeric(data$wind_speed)
     data$rain <-       as.numeric(data$rain)
-    if (length(which(is.na(data$mean_temp))) > 0)
-      data[is.na(data$mean_temp), ]$mean_temp  <- mean(data$mean_temp,  na.rm = TRUE)
-    if (length(which(is.na(data$wind_speed))) > 0)
-      data[is.na(data$wind_speed),]$wind_speed <- mean(data$wind_speed, na.rm = TRUE)
-    if (length(which(is.na(data$rain))) > 0)
-      data[is.na(data$rain),      ]$rain       <- mean(data$rain,       na.rm = TRUE)
     
+    # Append the data
     if (it == 0) {
       meteo.data <- data
     } else {
@@ -59,6 +58,7 @@ download_meteo_data <- function() {
     }
     it <- it + 1
     
+    # Next iteration
     month(current) <- month(current) + 1
     
     if (current > to) {
@@ -66,6 +66,13 @@ download_meteo_data <- function() {
     }
   }
   
-  meteo.data <- meteo.data[order(meteo.data$date),]
+  # Transform to data.table and fix NA with closest row
+  meteo.data <- as.data.table(meteo.data)
+  setkey(meteo.data, date)
+  meteo.data <- fix_NAs_closest_value(meteo.data, "mean_temp")
+  meteo.data <- fix_NAs_closest_value(meteo.data, "wind_speed")
+  meteo.data <- fix_NAs_closest_value(meteo.data, "rain")
+  
+  # Write the results to file
   write.table(meteo.data, "dat/meteo_hist.tsv", sep = "\t", row.names = FALSE)
 }
