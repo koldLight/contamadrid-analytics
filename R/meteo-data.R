5f870715960244d6b14728148213ff192b82e2da
Luz Frias <luzfrias@gmail.com>
2016-07-06 18:39:46 +0200

    Added relative humidity to meet data

--- R/meteo-data.R
+++ R/meteo-data.R
@@ -47,19 +47,20 @@ download_meteo_data <- function() {
     # Rarely the html table has an extra column
     table.ncol <- ncol(meteo.table)
     if (table.ncol == 23) {
-      col.ind <- c(1,4,8,11)
+      col.ind <- c(1,4,6,8,11)
     } else if (table.ncol == 22) {
-      col.ind <- c(1,4,8,10)
+      col.ind <- c(1,4,6,8,10)
     } else {
       stop(paste("Unrecognized table format in ", url))
     }
     
     data <- meteo.table[, col.ind]
-    colnames(data) <- c("date", "mean_temp", "wind_speed", "rain")
+    colnames(data) <- c("date", "mean_temp", "rel_humidity_pct", "wind_speed", "rain")
     
     # Transform it
     data$date <- as.Date(paste0(data$date, "/", year(current)), "%d/%m/%Y")
     data$mean_temp  <- as.numeric(data$mean_temp)
+    data$rel_humidity_pct  <- as.numeric(data$rel_humidity_pct)
     data$wind_speed <- as.numeric(data$wind_speed)
     data$rain <-       as.numeric(data$rain)
     
@@ -83,6 +84,7 @@ download_meteo_data <- function() {
   meteo.data <- as.data.table(meteo.data)
   setkey(meteo.data, date)
   meteo.data <- fix_NAs_closest_value(meteo.data, "mean_temp")
+  meteo.data <- fix_NAs_closest_value(meteo.data, "rel_humidity_pct")
   meteo.data <- fix_NAs_closest_value(meteo.data, "wind_speed")
   meteo.data <- fix_NAs_closest_value(meteo.data, "rain")
   
