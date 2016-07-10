4a29a2f6db357a8395deaec4ba706fb4ce703591
Luz Frias <luzfrias@gmail.com>
2016-07-10 23:24:23 +0200

    Added min and max temperature to meet data

--- R/meteo-data.R
+++ R/meteo-data.R
@@ -47,18 +47,21 @@ download_meteo_data <- function() {
     # Rarely the html table has an extra column
     table.ncol <- ncol(meteo.table)
     if (table.ncol == 23) {
-      col.ind <- c(1,4,6,8,11)
+      col.ind <- c(1,2,3,4,6,8,11)
     } else if (table.ncol == 22) {
-      col.ind <- c(1,4,6,8,10)
+      col.ind <- c(1,2,3,4,6,8,10)
     } else {
       stop(paste("Unrecognized table format in ", url))
     }
     
     data <- meteo.table[, col.ind]
-    colnames(data) <- c("date", "mean_temp", "rel_humidity_pct", "wind_speed", "rain")
+    colnames(data) <- c("date", "max_temp", "min_temp","mean_temp", "rel_humidity_pct",
+                        "wind_speed", "rain")
     
     # Transform it
     data$date <- as.Date(paste0(data$date, "/", year(current)), "%d/%m/%Y")
+    data$max_temp  <- as.numeric(data$max_temp)
+    data$min_temp  <- as.numeric(data$min_temp)
     data$mean_temp  <- as.numeric(data$mean_temp)
     data$rel_humidity_pct  <- as.numeric(data$rel_humidity_pct)
     data$wind_speed <- as.numeric(data$wind_speed)
@@ -83,6 +86,8 @@ download_meteo_data <- function() {
   # Transform to data.table and fix NA with closest row
   meteo.data <- as.data.table(meteo.data)
   setkey(meteo.data, date)
+  meteo.data <- fix_NAs_closest_value(meteo.data, "max_temp")
+  meteo.data <- fix_NAs_closest_value(meteo.data, "min_temp")
   meteo.data <- fix_NAs_closest_value(meteo.data, "mean_temp")
   meteo.data <- fix_NAs_closest_value(meteo.data, "rel_humidity_pct")
   meteo.data <- fix_NAs_closest_value(meteo.data, "wind_speed")
